ab59d4e78496ad1eaa04d602d137ba0923ee6243
ちんぱん <52692512+chinpanGX@users.noreply.github.com>
2025-05-25 00:29:35 +0900

    - `JsonHelper`クラスを新規追加し、JSON文字列の配列デシリアライズを共通化 - `AddressableMasterDataLoader`クラスを新規追加し、Addressablesのロード処理を分離 - `MasterDataTextAssetLoaderBase`クラス内のロードロジックをリファクタリングし、`JsonHelper`と`AddressableMasterDataLoader`を利用するよう変更 - 不必要なコードとフィールドを削除し、コードの簡素化と保守性を向上

---
+++ MasterSheetsUnityProject/Assets/MasterSheetz/Runtime/AddressableMasterDataLoader.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+namespace MasterSheetz.Runtime
+{
+    internal class AddressableMasterDataLoader
+    {
+        private readonly List<AsyncOperationHandle<TextAsset>> handles = new();
+
+        internal async UniTask<string> LoadAsync<T>(string masterDataAssetKey)
+        {
+            var handle = Addressables.LoadAssetAsync<TextAsset>(masterDataAssetKey);
+            await handle.ToUniTask();
+            if (!handle.Result.text.Contains("data"))
+            {
+                throw new MasterDataException("Invalid master data format.");
+            }
+            handles.Add(handle);
+            return handle.Result.text;
+        }
+
+        internal void Release()
+        {
+            foreach (var handle in handles)
+            {
+                Addressables.Release(handle);
+            }
+            handles.Clear();
+        }
+    }
+}
